9f37d872ed4c45f22cbcae32998462bd623c8429
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-07-19 13:41:34 -0400

    ResumePage updates

--- src/components/main/Resume.js
+++ src/components/main/Resume.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Entry from './Entry'
 import '../../styles/Resume.css';
+import ResumePDF from '../../resume/stephen-cases_resume.pdf'
 
  const Resume = (props) => {
 
@@ -8,13 +9,21 @@ import '../../styles/Resume.css';
 
     return(
         <div id="Resume">
+            <div className="dl-resume">
+                <a href={ResumePDF} className="resume-btn" download>Download Resume</a><br/>
+            </div>
             <div className="resume-header">
                 <h1>STEPHEN CASES</h1>
-                <h4>LOCATION: New York, NY</h4>
+                {/* <p className="resume-subheader">
+                    <b>Email:</b>&nbsp;stephen.cases@gmail.com  |  <b>Location:</b>&nbsp;New York, NY
+                </p> */}
             </div>
-            <ul>
-                {props.entries.map(entry => <Entry key={entry.id} {...entry} /> )}
-            </ul>
+            <div className="work-experience">
+                <h2 className="resume-experience">RELATED EXPERIENCE</h2>
+                <ul>
+                    {props.entries.map(entry => <Entry key={entry.id} {...entry} /> )}
+                </ul>
+            </div>
         </div>
     )
 }
