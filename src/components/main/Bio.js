467af4a5313f35e5db21cd5bdbe7e005a84d3295
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-07-23 10:56:14 -0400

    Bio Updated

--- src/components/main/Bio.js
+++ src/components/main/Bio.js
@@ -11,23 +11,17 @@ import '../../styles/Bio.css';
                 <h2>A <s className="little-strike">Little</s> Lot About Me...</h2>
                 <p className="bio-content">
                     First and foremost, thank you for visiting my site. I appreciate your time and interest.
-                </p>
-                <p className="bio-content">
+                    <br/><br/>                    
                     I've always been fascinated with Web Devlopment. Though initially pursuing a career in medicine to please my parents, I knew early on that the internet was the way of the future. Little did I know how much it would inevitably change my life.
-                </p>
-                <p className="bio-content">
+                    <br/><br/>                    
                     After college, I was working as a Lab Tech in Florida when I decided to take web development seriously. I taught myself the basics through video tutorials and whatever I could find on w3schools. Before I knew it, I was building simple web pages for my friends and a few personal clients.
-                </p>
-                <p className="bio-content">
+                    <br/><br/>                    
                     I eventually decided to take the leap and worked as a Jr. Developer for a digital studio of a well-known ad agency in New York City. The pay wasn't great but the experience was worth it. I worked on a different project every week, exposing myself to more hands on opportunites with HTML, CSS, and JavaScript. However, as the growing demand for email development grew, I was quickly appointed as the point of contact for all digital marketing campaigns.
-                </p>
-                <p className="bio-content">
+                    <br/><br/>                    
                     I ended up having a fairly successful career in the niche world of digital marketing. I gained a wealth of experience working for some well-known companies (<i>I won't bore you with the name-drops. You can just view my resume page</i>). Unfortunately, I was laid off just around the time the pandemic hit. 
-                </p>
-                <p className="bio-content">
+                    <br/><br/>                    
                     So, what to do? I had several opportunites lined up to continue my career with digital marketing, however, something didn't quite add up when I thought about my overall happiness with the career path I chose. After weighing my options, I decided to take another leap of faith and enrolled into the Flatiron School's Software Engineering program. For the next hundred-plus days, I was immersed in the world of full-stack development - learning core concepts of object-oriented programming, backend languages and frameworks such as Ruby on Rails, frontend languages and frameworks like JavaScript and ReactJS, as well as RESTful API and more!
-                </p>
-                <p className="bio-content">
+                    <br/><br/>                    
                     Now, I'm ready and eager to start this new career path. Having over a decade of experience with frontend development, coupled with my new found skills with software engineering, I feel that the sky's the limit!
                 </p>
                 <br/>
