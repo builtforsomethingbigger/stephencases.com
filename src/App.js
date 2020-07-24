0e4ba3dd212971ac6aff071aa72cd762fb8c12d3
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-07-24 16:47:37 -0400

    Changed APIs to Heroku backend

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import Resume from './components/main/Resume'
 import Contact from './components/main/Contact'
 import './styles/App.css';
 
-const resumeAPI = 'http://localhost:3001/resumes'
-const bioAPI = 'http://localhost:3001/bios'
-const portfolioAPI = 'http://localhost:3001/portfolios'
+const resumeAPI = 'http://www.stephen-cases-backend.herokuapp.com/resumes'
+const bioAPI = 'http://www.stephen-cases-backend.herokuapp.com/bios'
+const portfolioAPI = 'http://www.stephen-cases-backend.herokuapp.com/portfolios'
 
 export default class App extends React.Component{
 
