c106cd8988f8f6897c2526789c1c623382cad35b
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-08-08 15:41:25 -0400

    Social Icons hover update

--- src/components/main/vlog/apis/youtube.js
+++ src/components/main/vlog/apis/youtube.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // this is a public api key so it is ok to let it be unrestricted
-const KEY = 'AIzaSyAHUQQArxzKvILPFCzz9gI58GFnJrlDSJI'
+const KEY = 'AIzaSyCrqXGU3lAJpXnCJIo8Di-nQS8tx0yqmK0'
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/',
