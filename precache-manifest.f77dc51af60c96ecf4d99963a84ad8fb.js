2288575f908d78938facf2653110d10376b15950
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-08-21 22:34:21 -0400

    Updates

---
+++ precache-manifest.f77dc51af60c96ecf4d99963a84ad8fb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8da4b55cfe7088eaea67e2249b56c36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47c7e98048d8c47613f4",
+    "url": "/static/css/main.c844aace.chunk.css"
+  },
+  {
+    "revision": "2503b22afc14ac75c199",
+    "url": "/static/js/2.a9b49e8a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a9b49e8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47c7e98048d8c47613f4",
+    "url": "/static/js/main.57edba44.chunk.js"
+  },
+  {
+    "revision": "957c08b2231c2022d43e",
+    "url": "/static/js/runtime-main.d814ee9c.js"
+  },
+  {
+    "revision": "01e3a48c4859217fc1df032ad49fe8fb",
+    "url": "/static/media/Headshot_2018.01e3a48c.jpg"
+  },
+  {
+    "revision": "431bec2a372fcc0726841ad68fabfd33",
+    "url": "/static/media/github-icon.431bec2a.png"
+  },
+  {
+    "revision": "6bd61706386e48602dd567f37ad38551",
+    "url": "/static/media/linkedin-icon.6bd61706.png"
+  },
+  {
+    "revision": "cbedcb657e7bcd7d2cfb8d9a7006f91d",
+    "url": "/static/media/medium-icon.cbedcb65.png"
+  },
+  {
+    "revision": "a2a7a6566e469eeb4ded1307dc265d87",
+    "url": "/static/media/stephen-cases_resume.a2a7a656.pdf"
+  },
+  {
+    "revision": "06ff1f299c751acb94d195a52645e8fc",
+    "url": "/static/media/twitter-icon.06ff1f29.png"
+  }
+]);
